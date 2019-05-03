c3c71ab108afefd391b4d3dca1a4e71c3ccb30fc
alejo-rey <771@holbertonschool.com>
2019-05-03 18:18:08 +0000

    work with argumentss in python

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,1 +1,13 @@
 #!/usr/bin/python3
+if __name__ == "__main__":
+    import sys
+    l = len(sys.argv) - 1
+    if l == 1:
+        str1 = "argument:"
+    elif l == 0:
+        str1 = "arguments."
+    else:
+        str1 = "arguments:"
+    print("{:d} {}".format(l, str1))
+    for x in range(1, l + 1):
+        print("{:d}: {}".format(x, sys.argv[x]))
