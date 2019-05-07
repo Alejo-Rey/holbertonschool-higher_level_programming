2349d3acae93f00a5d99e22a3a5f32df92e7e38c
Alejo-rey <771@holbertonschool.com>
2019-05-07 19:30:55 +0000

    print a matrix

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,2 +1,10 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
+    if matrix is not None:
+        w = len(matrix)
+
+        for x in range (w):
+            y = len(matrix[x])
+            for z in range(y):
+                print("{:d}".format(matrix[x][z]), end=" ")
+            print()
