1631e6c5d3e179229078c555decad007d178a06c
Alejo-rey <771@holbertonschool.com>
2019-05-07 21:49:26 +0000

    return true if the number is multiple of 2

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -1,11 +1,10 @@
 #!/usr/bin/python3
 def divisible_by_2(my_list=[]):
-    if len(my_list) == 0:
-        return (None)
-    my_list2 = my_list.copy()
-    for x in range(len(my_list) - 1):
-        if my_list[x] % 2 == 0:
-            my_list2[x] = True
-        else:
-            my_list2[x] = False
-    return (my_list2)
+    if my_list is not None:
+        my_list2 = my_list.copy()
+        for x in range(len(my_list) - 1):
+            if my_list[x] % 2 == 0:
+                my_list2[x] = True
+            else:
+                my_list2[x] = False
+        return (my_list2)
