19b3093585bbe1555d1e7a7c0c910d254e7c7115
Alejo-rey <771@holbertonschool.com>
2019-05-07 17:56:47 +0000

    add a new element in the copy of the list

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,2 +1,8 @@
 #!/usr/bin/python3
 def new_in_list(my_list, idx, element):
+    x = len(my_list) - 1
+    if idx > x or idx < 0:
+        return (my_list.copy())
+    copy_list = my_list.copy()
+    copy_list[idx] = element
+    return (copy_list)
