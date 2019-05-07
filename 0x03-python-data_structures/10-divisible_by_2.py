6f452859604d0ccadb8be8aefa69a5a535273b82
Alejo-rey <771@holbertonschool.com>
2019-05-07 23:22:59 +0000

    return true if the number is multiple of 2

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -2,7 +2,7 @@
 def divisible_by_2(my_list=[]):
     if my_list is not None:
         my_list2 = []
-        for x in range(len(my_list) - 1):
+        for x in range(len(my_list)):
             if my_list[x] % 2 == 0:
                 my_list2.append(True)
             else:
