9b0ea0477f4aff140b3991b4ab8848101f598f10
Alejo-rey <771@holbertonschool.com>
2019-05-07 20:45:03 +0000

    add the elements of the tuples

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,2 +1,10 @@
 #!/usr/bin/python3
 def add_tuple(tuple_a=(), tuple_b=()):
+    a, b = len(tuple_a), len(tuple_b)
+
+    if a == 2 == b or a > 2 or b > 2:
+        return ((tuple_a[0] + tuple_b[0]), (tuple_a[1] + tuple_b[1]))
+    if b == 1:
+        return ((tuple_a[0] + tuple_b[0]), (tuple_a[1] + 0))
+    if b == 0:
+        return ((tuple_a[0] + 0), (tuple_a[1] + 0))
