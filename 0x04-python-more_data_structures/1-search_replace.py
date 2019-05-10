27549fe350fb0d42e801cd86fd836d15c493fd39
Alejo-Rey <771@holbertonschool.com>
2019-05-10 03:15:40 +0000

    serach and replace from a list

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,2 +1,6 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
+    if my_list is None:
+        return
+    copy = (list(map((lambda x: x if x != search else replace), my_list)))
+    return (copy)
