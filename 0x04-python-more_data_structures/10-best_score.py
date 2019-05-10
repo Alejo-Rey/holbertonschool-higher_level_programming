a641939d8dca78f079551688a8611c15fb3edb3c
Alejo-Rey <771@holbertonschool.com>
2019-05-10 05:20:23 +0000

    return the best score from a dictionary

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
-    if a_dictionary is None:
+    if a_dictionary is None or len(a_dictionary) == 0:
         return
     return (sorted(a_dictionary)[-1])
