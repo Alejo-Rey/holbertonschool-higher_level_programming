85c3a61398be8090855d4d722a6050a968985ea2
Alejo-rey <771@holbertonschool.com>
2019-05-20 22:52:43 +0000

    [feature] print the safe mode of int

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -1,2 +1,13 @@
 #!/usr/bin/python3
 def safe_print_list_integers(my_list=[], x=0):
+    z = 0
+    for y in my_list:
+        if z != x:
+            try:
+                z = z + 1
+                print("{:d}".format(y), end="")
+            except (TypeError, ValueError):
+                z -= 1
+                pass
+    print()
+    return (z)
