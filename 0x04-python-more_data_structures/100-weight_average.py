86447fd17753fa056a24eeed0a8230887611f99b
Alejo-Rey <771@holbertonschool.com>
2019-06-10 12:10:20 +0000

    first commit

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -1,2 +1,13 @@
 #!/usr/bin/python3
 def square_matrix_map(matrix=[]):
+    if matrix is None or len(matrix) == 0:
+        return (0)
+    d = 0
+    size = len(matrix) - 1
+    for a in range(size):
+        for b in range(a):
+            c = matrix[a] * [b]
+            d += matrix[b]
+        e += c
+
+    return (e)
