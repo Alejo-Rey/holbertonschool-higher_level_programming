17f63b2143db76e4c9528e1d12fb342c84da30b0
Alejo-rey <771@holbertonschool.com>
2019-06-11 08:23:30 +0000

    class square

---
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+
+from models.rectangle import Rectangle
+
+class Square(Rectangle):
+    def __init__(self, size, x=0, y=0, id=None):
+        super().__init__(size, size, x, y, id)
+
+    @property
+    def size(self):
+        return super().width
+
+    @size.setter
+    def size(self, value):
+        super(Square, self.__class__).width.fset(self, value)
+        super(Square, self.__class__).height.fset(self, value)
+
+    def __str__(self):
+         return (
+                "[Square] (" +
+                str(self.id) +
+                ") " +
+                str(self.x) +
+                "/" +
+                str(self.y) +
+                " - " +
+                str(self.size)
+                )
