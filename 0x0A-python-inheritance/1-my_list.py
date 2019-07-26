de0f043049e3e0becbf36d5885abde5d19ebc869
Alejo-rey <771@holbertonschool.com>
2019-07-26 13:01:18 +0000

    function to print sorted

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -1,12 +1,9 @@
 #!/usr/bin/python3
-''' funtion to print a list inherit '''
+''' programt to list sorted list '''
 
 
 class MyList(list):
-    ''' Class Mylist to print sorted '''
-    def __init__(self):
-        list.__init__(self)
-
+    ''' class to print sorted list '''
     def print_sorted(self):
-        ''' function to return a sorted list '''
         print(sorted(self))
+
