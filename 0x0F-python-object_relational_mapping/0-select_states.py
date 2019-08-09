2ce22c3a586c240ff5d37a1ca78f4631408f771d
Alejo-rey <771@holbertonschool.com>
2019-08-09 20:33:32 +0000

    [FIX] requirements

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,15 +1,19 @@
 #!/usr/bin/python3
 ''' program to run mysqldb in mysql '''
-import sys
-import MySQLdb
+if __name__ == "__main__":
+    import sys
+    import MySQLdb
 
-''' connection to database '''
+    ''' connection to database '''
+    db = MySQLdb.connect(host="localhost",
+                         port=3306,
+                         user=sys.argv[1],
+                         passwd=sys.argv[2],
+                         database=sys.argv[3])
+    cursor = db.cursor()
+    cursor.execute("select * from states")
 
-db = MySQLdb.connect("localhost",sys.argv[1],sys.argv[2],sys.argv[3])
-cursor = db.cursor()
-cursor.execute("select * from states")
-
-for data in cursor:
-    print(data)
-''' close the connection '''
-db.close()
+    for data in cursor:
+        print(data)
+    ''' close the connection '''
+    db.close()
