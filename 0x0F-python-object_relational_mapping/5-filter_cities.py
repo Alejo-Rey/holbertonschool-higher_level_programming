841b9176ca01fd27739cd469c4f83767916738e6
Alejo-rey <771@holbertonschool.com>
2019-08-10 00:52:01 +0000

    [FEATURE] cript that takes in the name of a state as an argument and lists all cities

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -1,1 +1,27 @@
 #!/usr/bin/python3
+''' program to run mysqldb in mysql '''
+if __name__ == "__main__":
+    import sys
+    import MySQLdb
+
+    ''' connection to database '''
+    db = MySQLdb.connect(host="localhost",
+                         port=3306,
+                         user=sys.argv[1],
+                         passwd=sys.argv[2],
+                         database=sys.argv[3])
+    cursor = db.cursor()
+    cursor.execute(
+            "SELECT cities.name \
+                    FROM cities \
+                    JOIN states ON cities.state_id = states.id \
+                    WHERE states.name = %(id)s", {'id': sys.argv[4]}
+            )
+
+    List = []
+    for data in cursor.fetchall():
+        for x in data:
+            List.append(x)
+    print(', '.join(List))
+    ''' close the connection '''
+    db.close()
