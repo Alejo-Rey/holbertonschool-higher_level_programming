a5b4987482c10c54c4d4b1b582aa703aba76c438
Alejo-Rey <771@holbertonshcool.com>
2019-08-11 18:45:02 +0000

    [FEATURE] show the data with ORM

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -1,1 +1,26 @@
 #!/usr/bin/python3
+''' list objects from State '''
+
+import sys
+from model_state import Base, State
+from sqlalchemy import create_engine
+from sqlalchemy import Table, Column
+from sqlalchemy.orm import sessionmaker
+
+
+if  __name__ == '__main__':
+    ''' Create the engine '''
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
+                           .format(sys.argv[1],
+                                   sys.argv[2],
+                                   sys.argv[3]),
+                           pool_pre_ping=True)
+
+    Base.metadata.bind = engine
+    DBSession = sessionmaker()
+    DBSession.bind = engine
+    session = DBSession()
+    for data in session.query(State).all():
+        print('{}: {}'.format(data.id, data.name))
+    
+    
