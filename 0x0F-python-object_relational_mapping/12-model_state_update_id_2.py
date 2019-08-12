bf6664058c708ea11e18586d732128632dc79b42
Alejo-Rey <771@holbertonschool.com>
2019-08-12 01:30:42 +0000

    [Feature] Update state name

--- 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
+++ 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
@@ -1,1 +1,25 @@
 #!/usr/bin/python3
+''' list objects from State '''
+
+import sys
+from model_state import Base, State
+from sqlalchemy import create_engine, update
+from sqlalchemy.orm import sessionmaker
+
+
+if __name__ == '__main__':
+    ''' Create the engine '''
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
+                           .format(sys.argv[1],
+                                   sys.argv[2],
+                                   sys.argv[3]),
+                           pool_pre_ping=True)
+    Base.metadata.bind = engine
+    DBSession = sessionmaker()
+    DBSession.bind = engine
+    session = DBSession()
+
+    new_mex = session.query(State).filter(State.id == 2).first()
+
+    new_mex.name = 'New Mexico'
+    session.commit()
