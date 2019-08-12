eaa7d56933cdfa9327f747cd436a0cff24b1b54d
Alejo-Rey <771@holbertonschool.com>
2019-08-12 04:53:57 +0000

    second commit

--- 0x0F-python-object_relational_mapping/relationship_state.py
+++ 0x0F-python-object_relational_mapping/relationship_state.py
@@ -1,14 +1,14 @@
 #!/usr/bin/python3
 ''' use ORM MySQLAlchemy '''
-from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
-from model_state import Base, State
+
+Base = declarative_base()
 
 
-class City(Base):
+class State(Base):
     ''' class to create the table state '''
-    __tablename__ = 'cities'
+    __tablename__ = 'states'
 
     id = Column(Integer, primary_key=True, nullable=False)
     name = Column(String(128), nullable=False)
-    state_id = Column(Integer, ForeignKey('states.id'), nullable=False)
