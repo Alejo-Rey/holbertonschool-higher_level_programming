ff9825beacf2a81bd7f9e8ab7a8e23ef284cc612
Alejo-Rey <771@holbertonschool.com>
2019-08-12 02:08:15 +0000

    [FEATURE] delete a row in a table

--- 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
+++ 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
@@ -1,1 +1,25 @@
 #!/usr/bin/python3
+''' list objects from State '''
+
+import sys
+from model_state import Base, State
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+
+if __name__ == '__main__':
+    ''' Create the engine '''
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
+                           .format(sys.argv[1],
+                                   sys.argv[2],
+                                   sys.argv[3]),
+                           pool_pre_ping=True)
+
+    Base.metadata.bind = engine
+    DBSession = sessionmaker()
+    DBSession.bind = engine
+    session = DBSession()
+    data = session.query(State).filter(State.name.like('%a%'))
+    for x in data:
+        session.delete(x)
+    session.commit()
