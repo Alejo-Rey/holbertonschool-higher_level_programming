c8c75694cd65db4e037d4d0e274aecc0a24e0a43
Alejo-Rey <771@holbertonschool.com>
2019-08-21 00:07:30 +0000

    [FEATURE] send a post method in curl

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # script to send a post method with values
-curl -sd "email: hr@holbertonschool.com" -sd "subject: I will always be here for PLD" $1
+curl -sd "email: hr@holbertonschool.com&subject: I will always be here for PLD" $1
