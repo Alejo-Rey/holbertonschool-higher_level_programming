e5438625c59ce8dd80095323365441602ce6985c
Alejo-rey <771@holbertonschool.com>
2019-08-31 03:18:51 +0000

    [FEATURE] response the value of the specific header

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -4,5 +4,8 @@
 import requests
 import sys
 
-req = requests.get(sys.argv[1])
-print("{}".format(req.headers.get('X-Request-Id')))
+try:
+    req = requests.get(sys.argv[1])
+    print("{}".format(req.headers.get('X-Request-Id')))
+except urllib.error.HTTPError as e:
+    print("Error code: {}".format(e.code))
