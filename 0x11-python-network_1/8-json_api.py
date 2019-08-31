c3736e272ab40f9c2868b5d8b4806ac5f8f6e4d4
Alejo-rey <771@holbertonschool.com>
2019-08-31 10:23:55 +0000

    [FEATURE] send POST json

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -6,12 +6,15 @@ import sys
 
 url = 'http://0.0.0.0:5000/search_user'
 try:
-    if (len(sys.argv[1]) == 0):
+    if (len(sys.argv) == 1):
         data = {'q': ''}
+        print(data)
     else:
         data = {'q': sys.argv[1]}
+        print(data)
     req = requests.post(url, data=data)
     if (len(req.json()) == 0):
+        print(req.json())
         print('No result')
     else:
         print('[{}] <{}>'.format(req.json().get('id'), req.json().get('name')))
