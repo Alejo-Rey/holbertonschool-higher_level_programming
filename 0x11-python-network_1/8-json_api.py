5f507d1b65d9f9818bea75ccf050422512f7d86c
Alejo-rey <771@holbertonschool.com>
2019-08-31 08:48:05 +0000

    [FEATURE] send POST json

---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+''' use the requests library '''
+
+import requests
+import sys
+
+url = 'http://0.0.0.0:5000/search_user'
+try:
+    if (len(sys.argv[1]) == 0):
+        data = {'q': ''}
+    else:
+        data = {'q': sys.argv[1]}
+    req = requests.post(url, data=data)
+    if (len(req.json()) == 0):
+        print('No result')
+    else:
+        print('[{}] <{}>'.format(req.json().get('id'), req.json().get('name')))
+except:
+        pass
