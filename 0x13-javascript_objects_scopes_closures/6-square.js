e105b9feeaed87022acbf4afb0dfa432338c2907
Alejo-rey <771@holbertonschool.com>
2019-09-24 06:07:05 +0000

    [FEATURE] inherit Square and create method charPrint

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -1,10 +1,20 @@
 #!/usr/bin/node
-/* function to export a class */
-const Rectangle = require('./4-rectangle');
+const Square2 = require('./5-square.js');
 
-class Square extends Rectangle {
-  constructor (size) {
-    super(size, size);
+class Square extends Square2 {
+  charPrint (c) {
+    if (c === undefined) {
+      super.print();
+    } else {
+      let x;
+      let row = '';
+      for (x = 0; x <= this.height; x++) {
+        row += 'C';
+      }
+      for (x = 0; x <= this.height; x++) {
+        console.log(row);
+      }
+    }
   }
 }
 module.exports = Square;
